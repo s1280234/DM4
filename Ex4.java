0e0a2b66539b8b3a13c637afeac1c058239a4e64
s1280234 <86813227+s1280234@users.noreply.github.com>
2021-07-02 12:31:32 +0900

    Greetings added

--- Ex4.java
+++ Ex4.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class Ex4{
     public static void main(String[] args){        
-     
-       int value1 = (int)(Math.random() * 6) + 1;
+     Scanner scanner = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String input_text = scanner.nextLine();
+    System.out.println("Hello," +input_text+ "!");
+       
+        int value1 = (int)(Math.random() * 6) + 1;
         System.out.println("Die1: " +value1);
         int value2 = (int)(Math.random() * 6) + 1;
         System.out.println("Die2: " +value2);
